fdb6d2f71fb279e13228c2fdccb07df7dc79b239
Jonntz <monteirojonatas23@gmail.com>
2024-03-20 16:56:48 -0300

    feat: Add Akira's text

---
+++ src/app/components/akira/akira.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-akira',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './akira.component.html',
+  styleUrl: './akira.component.scss'
+})
+export class AkiraComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { AkiraComponent } from '../../components/akira/akira.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, AkiraComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
