8f88c79bf6efd5132d209dbdf0b9dd411c8f87ba
Jonntz <monteirojonatas23@gmail.com>
2024-03-20 21:31:51 -0300

    some css config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: '',   redirectTo: 'home', pathMatch: 'full' }
+    { path: '', component: HomeComponent},
+    { path: '',   redirectTo: '', pathMatch: 'full' },
+
+    {path:'characters', component: CharactersComponent}
 ];
